cb3bf215128b997a85474d63db63f52e59df40c0
Anshu <anshuranjan32@gmail.com>
2020-10-16 23:43:55 +0530

    Extent report update and properties file

--- Cucumber/src/main/java/Runner/TestRunner.java
+++ Cucumber/src/main/java/Runner/TestRunner.java
@@ -7,12 +7,14 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "C:\\Users\\Anshu\\Test\\Cucumber\\src\\main\\java\\features"
+		features = "C:\\Users\\Anshu\\git\\CucumberFramework\\Cucumber\\src\\main\\java\\features\\"
 		,glue = {"stepDefinition"}
-		,plugin = {"pretty", "html:target/cucumber"}
-		,monochrome = true
-		
+		,plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+		//,plugin = { "pretty", "html:target/cucumber-reports/Cucumber.html" }
+		, monochrome = true
+		//	, dryRun = true
+		, publish = true
 		)
 public class TestRunner {
-
+ 
 }
